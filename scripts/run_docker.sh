fa9663dca88948efe10660104633a86a6f66b631
cml1010101 <115354809+cml1010101@users.noreply.github.com>
2023-07-10 13:35:21 -0400

    added volume mount of .vscode server (#5)

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -5,4 +5,5 @@ shift
 DEV_DIR=$(pwd)
 docker run --rm -it --privileged --network host --workdir /workspaces/rosnfr -v ${DEV_DIR}:/workspaces/rosnfr \
     -v ${HOME}/.Xauthority:/root/.Xauthority --runtime nvidia --env DISPLAY=${DISPLAY} \
+    -v ${HOME}/.vscode-server:/root/.vscode-server \
     --entrypoint /workspaces/workspace_entrypoint.sh ${IMAGE_NAME} /bin/bash $@
