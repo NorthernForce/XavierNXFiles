19acae5ac8291a9249e324fe020eb9f328b74221
cml1010101 <cml1010101@gmail.com>
2023-07-24 19:55:57 -0400

    named the container aarch64-xaveier-container by default

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -6,5 +6,5 @@ DEV_DIR=$(pwd)
 docker run --rm -it --privileged --network host --workdir /workspaces/rosnfr -v ${DEV_DIR}:/workspaces/rosnfr \
     -v ${HOME}/.Xauthority:/root/.Xauthority --runtime nvidia --env DISPLAY=${DISPLAY} \
     -v ${HOME}/.vscode-server:/root/.vscode-server \
-    -v /dev/*:/dev/* \
+    -v /dev/*:/dev/* --name aarch64-xavier-container \
     --entrypoint /workspaces/workspace_entrypoint.sh ${IMAGE_NAME} /bin/bash $@
