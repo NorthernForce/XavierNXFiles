f20b31dd5852b70565059a57223f2e6c6d81c1a1
cml1010101 <cml1010101@gmail.com>
2023-07-24 20:41:08 -0400

    created two new more specific scripts

---
+++ scripts/launch_docker_devel.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+IMAGE_NAME=$1
+shift
+DEV_DIR=$(pwd)
+docker run --rm -it --privileged --network host --workdir /workspaces/rosnfr -v ${DEV_DIR}:/workspaces/rosnfr \
+    -v ${HOME}/.Xauthority:/root/.Xauthority --runtime nvidia --env DISPLAY=${DISPLAY} \
+    -v ${HOME}/.vscode-server:/root/.vscode-server \
+    -v /dev/*:/dev/* --name aarch64-devel-container \
+    --entrypoint /workspaces/workspace_entrypoint.sh ${IMAGE_NAME} /bin/bash $@
