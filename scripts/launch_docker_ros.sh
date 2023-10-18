d217b4d9124a84190d769ac4d7501d04411318d8
cml1010101 <cml1010101@gmail.com>
2023-10-18 16:03:51 -0400

    Many Changes

--- scripts/launch_docker_ros.sh
+++ scripts/launch_docker_ros.sh
@@ -8,8 +8,8 @@ shift
 DEV_DIR=$(pwd)
 trap "echo Stopping aarch64-ros-container && docker stop aarch64-ros-container" INT
 docker run --rm -dit --privileged --network host --workdir /workspaces/rosnfr -v ${DEV_DIR}:/workspaces/rosnfr \
-    --runtime nvidia \
-    -v /dev/*:/dev/* --name aarch64-ros-container \
+    -v ${HOME}/.Xauthority:/root/.Xauthority --runtime nvidia --env DISPLAY=${DISPLAY} \
+    -v /dev/*:/dev/* --name aarch64-ros-container -v /tmp/.X11-unix:/tmp/.X11-unix \
     --entrypoint /workspaces/workspace_entrypoint.sh ${IMAGE_NAME} /bin/bash && \
     docker exec aarch64-ros-container /bin/bash -c "source install/setup.bash && RMW_IMPLEMENTATION=rmw_cyclonedds_cpp ros2 launch ${PACKAGE_NAME} ${LAUNCH_FILE} $@" && \
     docker stop aarch64-ros-container
